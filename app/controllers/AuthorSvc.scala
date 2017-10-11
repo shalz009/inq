0b10f928cf52bbad7416ad2218a6c35a1da5cfee
ayirnna <navanitha.rao.ayirneni@one.verizon.com>
2017-10-11 15:02:58 -0700

    updated

--- app/controllers/AuthorSvc.scala
+++ app/controllers/AuthorSvc.scala
@@ -1,16 +1,23 @@
 package controllers
 
-import play._
+import play.api._
+import play.api.mvc._
 import java.util.UUID
 import services.logic.AuthorHandler;
 import scala.concurrent.Future
 
-object AuthorSvc extends Controller {
+import javax.inject.Singleton
+
+@Singleton
+class AuthorController extends Controller {
 
   def create(name: String) = Action {
 
     AuthorHandler.handle(name)
-    OK(200)
+
+    Ok("hello")
+
+
 
   }
 }
